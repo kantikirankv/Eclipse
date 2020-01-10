f7755a24c628d2cad804f515eedebcfdb0168957
kantikirankv <kantikiran.kv@gmail.com>
2020-01-11 10:01:05 +1100

    first commit - checking in project in GIT

---
+++ Git_Checkin/src/pkg/Check_In.java
@@ -0,0 +1,45 @@
+package pkg;
+
+public class Check_In {
+
+	public static void main(String[] args) {
+		System.out.println("Git Checkin 1");
+	}
+
+}
+
+/*
+
+1. How to create github repository
+2. How to clone repository in eclipse
+3. How to add eclipse project to github repository
+4. How to commit, push and pull the changes
+
+Step 1 : Create GitHub account and SignIn
+Step 2 : Start a Project = Create a repository
+
+//https://github.com/kantikirankv/Eclipse.git - create repository(Project) in GIT
+ * 
+ * C:\Users\Trinath\eclipse-workspace\Git_Checkin - Java project in Eclipse
+ * 
+ * C:\Users\Trinath\git\Eclipse\.git - 'Eclipse' rep - git repository checkout in Eclipse
+ * 
+Step 3 : In Eclipse, Goto Perspective - (search) Git Repositories and click on clone a git Repositories
+The repository from git is loaded to Eclipse
+To show Java and git perspective in the same window - in eclipse - window - show view - other - Git repositorys
+
+Step 5 : Create a new Java project in Eclipse
+Step 6 : Do a right click on Project - Team - Share - Adds the new java prj to local eclipse git repo
+
+Step 7 :  Do a right click on Project - Team - Commit- move all the required changes from 'un-staged' to 'staged' - give comments-
+and commit and push /commit - the project to the repo
+Step 8 : Commit and Push every change to the repo
+
+
+
+
+*/
+
+
+
+
